b118af272c27ba1546c26deb4740f778f454487d
James George <jamesgeorge21@gmail.com>
2018-06-19 15:41:34 +0000

    Moved class to a package

---
+++ src/examples/class1/HelloWorld.java
@@ -0,0 +1,10 @@
+package examples.class1;
+
+public class HelloWorld {
+    
+    public static void main (String args[]) {
+        System.out.println("Hello World!");
+        System.out.println("Argument passed in on command line are " + args);
+        System.out.println("Length of array argument passed in on command line " + args.length);
+    }
+}
