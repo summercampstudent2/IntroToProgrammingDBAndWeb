04eb7e06c542c9a7d468969721ae38ae7a8549c8
Summer Camp Teacher 1 <summercamp.teacher1@gmail.com>
2018-06-24 03:38:34 +0000

    Additional demo files from Oracle Java tutorial

---
+++ src/examples/class2/ForDemo.java
@@ -0,0 +1,11 @@
+package examples.class2;
+
+
+
+class ForDemo {
+    public static void main(String[] args){
+         for(int i=1; i<11; i++){
+              System.out.println("Count is: " + i);
+         }
+    }
+}
---
+++ src/examples/class2/WhileDemo.java
@@ -0,0 +1,11 @@
+package examples.class2;
+
+class WhileDemo {
+    public static void main(String[] args){
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+}
